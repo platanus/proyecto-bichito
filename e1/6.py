2329d82b0d1b0e9a2e54224955343c12b24dd9b6
ttapia <tetapia@uc.cl>
2022-02-18 15:03:20 -0300

    docs(e1): added enunciado and removed common instructions from each exercise

--- e1/6.py
+++ e1/6.py
@@ -1,43 +1,5 @@
-#                                    ,__                   __
-#                                    '~~****Nm_    _mZ*****~~
-#                                            _8@mm@K_
-#                                           W~@`  '@~W
-#                                          ][][    ][][
-#                                    gz    'W'W.  ,W`W`    es
-#                                  ,Wf    gZ****MA****Ns    VW.
-#                                 gA`   ,Wf     ][     VW.   'Ms
-#                                Wf    ,@`      ][      '@.    VW
-#                                M.    W`  _mm_ ][ _mm_  'W    ,A
-#                                'W   ][  i@@@@i][i@@@@i  ][   W`
-#                                 !b  @   !@@@@!][!@@@@!   @  d!
-#                                  VWmP    ~**~ ][ ~**~    YmWf
-#                                    ][         ][         ][
-#                                  ,mW[         ][         ]Wm.
-#                                 ,A` @  ,gms.  ][  ,gms.  @ 'M.
-#                                 W`  Yi W@@@W  ][  W@@@W iP  'W
-#                                d!   'W M@@@A  ][  M@@@A W`   !b
-#                                @.    !b'V*f`  ][  'V*f`d!    ,@
-#                                'Ms    VW.     ][     ,Wf    gA`
-#                                  VW.   'Ms.   ][   ,gA`   ,Wf
-#                                   'Ms    'V*mmWWmm*f`    gA`
-
-###################################################################################################
-###################################################################################################
-##                                   Proyecto 2048: Entrega 1                                    ##
-##                                                                                               ##
-##                                 Variables y Control de Flujo                                  ##
-##                                                                                               ##
-###################################################################################################
-###################################################################################################
-
-# En esta primera entrega, vamos a implementar algunas funcionalidades más básicas del juego 2048.
-# En este juego, las piezas pueden tomar el valor de 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048.
-# Además, para ayudarnos a desarrollar el juego, vamos a asumir que una casilla vacía tiene un valor de 0.
-# Para que dos piezas puedan sumarse, deben tener el mismo valor.
-
-###################################################################################################
-
 # Ejercicio 6: Movimiento generalizado con dos celdas
+#
 # En este ejercicio vamos a mezclar la solución de los dos ejercicios anteriores. Es decir, vamos a tener algunos
 # casos en que habrá una celda vacía, vamos a tener algunos casos en que dos celdas pueden sumarse, y vamos a tener
 # algunos casos en que no se pueden sumar.
@@ -71,13 +33,3 @@ numero_2 = 64
 direccion = input("Escribe izquierda o derecha: ")
 
 ### Aquí abajo escribe tu código
-
-
-
-### Fin de tu código
-
-# Cuando hayas terminado tu código, ejecutalo escribiendo el siguiente comando en la consola:
-#
-# $ python 6.py
-#
-# ¡Puedes ejecutarlo las veces que quieras! Así podrás ir cambiando los valores de las variables y ver que sucede.
---
+++ e1/enunciado.md
@@ -0,0 +1,44 @@
+                                    ,__                   __
+                                    '~~****Nm_    _mZ*****~~
+                                            _8@mm@K_
+                                           W~@`  '@~W
+                                          ][][    ][][
+                                    gz    'W'W.  ,W`W`    es
+                                  ,Wf    gZ****MA****Ns    VW.
+                                 gA`   ,Wf     ][     VW.   'Ms
+                                Wf    ,@`      ][      '@.    VW
+                                M.    W`  _mm_ ][ _mm_  'W    ,A
+                                'W   ][  i@@@@i][i@@@@i  ][   W`
+                                 !b  @   !@@@@!][!@@@@!   @  d!
+                                  VWmP    ~**~ ][ ~**~    YmWf
+                                    ][         ][         ][
+                                  ,mW[         ][         ]Wm.
+                                 ,A` @  ,gms.  ][  ,gms.  @ 'M.
+                                 W`  Yi W@@@W  ][  W@@@W iP  'W
+                                d!   'W M@@@A  ][  M@@@A W`   !b
+                                @.    !b'V*f`  ][  'V*f`d!    ,@
+                                'Ms    VW.     ][     ,Wf    gA`
+                                  VW.   'Ms.   ][   ,gA`   ,Wf
+                                   'Ms    'V*mmWWmm*f`    gA`
+
+# Proyecto 2048: Entrega 1: Variables y Control de Flujo         
+
+En esta primera entrega, vamos a implementar algunas funcionalidades más básicas del juego 2048.
+
+### Reglas relevantes
+- En este juego, las piezas pueden tomar el valor de 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048.
+- Para que dos piezas puedan sumarse, deben tener el mismo valor.
+- Para ayudarnos a desarrollar el juego, vamos a asumir que una casilla vacía tiene un valor de 0.
+
+### Instrucciones para probar
+
+Cuando quieras probar tu código, ejecutalo escribiendo el siguiente comando en la consola:
+```
+python 1.py
+```
+Recuerda que puedes ejecutarlo las veces que quieras. Así podrás ir cambiando los valores de las variables y ver que sucede.
+Recuerda también cambiar el `1.py` por el número del ejercicio que estás desarrollando. Como por ejemplo:
+
+```
+python 5.py
+```
